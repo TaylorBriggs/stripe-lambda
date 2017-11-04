ed041720a3bfdfa9ee7265b5f9e496f40388b87e
Jon <jon@emissaryinc.com>
2017-11-04 15:20:53 -0700

    update to work with POST

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 require('dotenv').load();
+var qs = require('qs');
 
 var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = function(event, context) {
-
+  console.log("event: \n" + JSON.stringify(event, null, 4));
+  var data = qs.parse(event.body);
+  console.log("body: \n" + JSON.stringify(data, null, 4));
   stripe.charges.create({
-    amount:        event.amount,
-    source:        event.source,
-    currency:      event.currency || 'usd',
-    description:   event.description || 'Stripe payment '+event.order_id,
-    receipt_email: event.receipt_email || null
+    amount:        data.amount,
+    source:        data.source,
+    currency:      data.currency || 'usd',
+    description:   data.description || 'Stripe payment '+data.order_id,
+    receipt_email: data.receipt_email || null
   }, function(err, charge) {
     if (err && err.type === 'card_error') {
       context.fail(new Error(err.message));
